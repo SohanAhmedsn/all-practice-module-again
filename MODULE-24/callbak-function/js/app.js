e12bde3dd07d98896b7a90c55ccffcbe00757b63
sn77sohan <sohan.ahmedself@gmail.com>
2021-12-07 15:42:01 +0600

    111111111111

--- MODULE-24/callbak-function/js/app.js
+++ MODULE-24/callbak-function/js/app.js
@@ -1,16 +1,56 @@
-//Call back and higher order function 
+btnHandleToColorChangeRed = () => {
+  document.body.style.backgroundColor = "red";
+};
+btnHandleToColorChangeYellow = () => {
+  document.body.style.backgroundColor = "yellow";
+};
 
+///make blue btn
 
-function square(x) {
-    console.log(`square of ${x}: ${x * x}`);
+const blueButton = document.getElementById("make-blue-btn");
+blueButton.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+  document.body.style.color = "white";
 }
-square(8);
 
-// const y = square;
-// y(8);
-
-function higherOrderFucntion (num,callback) {
-
+const crimsonBtn = document.getElementById("make-crimson-btn");
+crimsonBtn.onclick = crimsonButton;
+function crimsonButton() {
+  document.body.style.backgroundColor = "crimson";
+  document.body.style.color = "white";
 }
 
-higherOrderFucntion(6, square)
+//make green
+
+const makeGreen = document.getElementById("make-green-btn");
+makeGreen.onclick = greenButton;
+function greenButton() {
+  document.body.style.backgroundColor = "green";
+}
+
+//make black
+
+const makeBlack = document.getElementById("make-black-btn");
+makeBlack.onclick = blackButton;
+function blackButton() {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
+
+// //Call back and higher order function
+
+// function square(x) {
+//     console.log(`square of ${x}: ${x * x}`);
+// }
+// square(8);
+
+// // const y = square;
+// // y(8);
+
+// function higherOrderFucntion (num,callback) {
+
+// }
+
+// higherOrderFucntion(6, square)
